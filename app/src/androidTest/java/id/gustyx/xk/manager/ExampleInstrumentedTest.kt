992ccaaa1bc2c58f0c72214def000e2f21654901
Gustyx-Power <gustiadityamuzaky08@gmail.com>
2025-01-28 20:04:42 +0700

    Refactor ExampleUnitTest & ExampleInstrumentedTest
    Signed-off-by: Gustyx-Power <gustiadityamuzaky08@gmail.com>

--- app/src/androidTest/java/id/gustyx/xk/manager/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/id/gustyx/xk/manager/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
-package id.gustyx.xk.manager
+package id.xtramanagersoftware.xk.manager
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("id.gustyx.xk.manager", appContext.packageName)
+        assertEquals("id.xtramanagersoftware.xk.manager", appContext.packageName)
     }
 }
