8e5ddc4663221d21258bccc75447353958beacc4
Gustyx-Power <gustiadityamuzaky08@gmail.com>
2025-02-01 01:34:31 +0700

    Xtra Kernel Manage : Remove java.io.RandomAccessFile in Module Import Class
    Signed-off-by: Gustyx-Power <gustiadityamuzaky08@gmail.com>

--- app/src/main/java/id/xtramanagersoftware/xk/manager/HomeActivity.kt
+++ app/src/main/java/id/xtramanagersoftware/xk/manager/HomeActivity.kt
@@ -17,7 +17,6 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
 import java.io.IOException
-import java.io.RandomAccessFile
 import java.lang.reflect.Method
 import java.util.concurrent.TimeUnit
 
