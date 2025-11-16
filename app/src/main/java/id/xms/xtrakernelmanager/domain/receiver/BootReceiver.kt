3e411a98f8bc0d0e60597fdee0f166b6304f980c
Gusti Aditya Muzaky <142430633+Gustyx-Power@users.noreply.github.com>
2025-11-17 05:58:24 +0700

    feat(boot): Add BootReceiver to apply settings on startup
    This commit introduces a `BootReceiver` that listens for the `ACTION_BOOT_COMPLETED` intent.
    
    When the device finishes booting, the receiver enqueues a `OneTimeWorkRequest` for the `ApplyConfigWorker` using `WorkManager`. This ensures that any saved configurations are applied automatically on startup.

---
+++ app/src/main/java/id/xms/xtrakernelmanager/domain/receiver/BootReceiver.kt
@@ -0,0 +1,17 @@
+package id.xms.xtrakernelmanager.domain.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import id.xms.xtrakernelmanager.domain.worker.ApplyConfigWorker
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val workRequest = OneTimeWorkRequestBuilder<ApplyConfigWorker>().build()
+            WorkManager.getInstance(context).enqueue(workRequest)
+        }
+    }
+}
