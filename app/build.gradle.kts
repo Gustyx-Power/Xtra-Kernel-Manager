38106e37b113f8100460b389c17f100435715b7a
Gusti Aditya Muzaky <142430633+Gustyx-Power@users.noreply.github.com>
2025-11-27 22:30:33 +0700

    chore(release): Bump version to 2.0-Beta1
    This commit updates the application's version name in the `build.gradle.kts` file.
    
    - **`versionName`**: Changed from `"2.0-Beta"` to `"2.0-Beta1"`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 2
-        versionName = "2.0-Beta"
+        versionName = "2.0-Beta1"
 
         vectorDrawables {
             useSupportLibrary = true
