1e1f26d039d91d5c5ba506f27b296b61f045b8a9
Gusti Aditya Muzaky <142430633+Gustyx-Power@users.noreply.github.com>
2025-12-12 06:52:49 +0700

    feat(deps): Add Lottie animation library
    This commit introduces the Lottie library for Android to enable rich, vector-based animations within the application.
    
    ### `build.gradle.kts`
    - The `lottie-compose` dependency (`com.airbnb.android:lottie-compose:6.4.0`) has been added to the project's dependencies.
    - The `versionName` has been updated from `2.0-Release` to `2.2-ReleaseTest-ci-3`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 2
-        versionName = "2.0-Release"
+        versionName = "2.2-ReleaseTest-ci-3"
 
         // Build date in format YYYY.MM.dd
         val buildDate = SimpleDateFormat("yyyy.MM.dd").format(Date())
@@ -129,6 +129,9 @@ dependencies {
 
     // Coil for image loading
     implementation("io.coil-kt:coil-compose:2.7.0")
+
+    // Lottie for animations
+    implementation("com.airbnb.android:lottie-compose:6.4.0")
 
     // Charts
     implementation("com.patrykandpatrick.vico:compose-m3:2.0.0-alpha.28")
