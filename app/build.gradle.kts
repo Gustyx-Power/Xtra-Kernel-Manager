35f221b2f9c1ad14f51dfcbbd89a08878ac387f7
pavelc4 <dimzzy23@gmail.com>
2025-12-26 09:46:50 +0700

    chore: Upgrade Gradle, AGP, and Kotlin plugins, set Java/Kotlin target to 21, and enhance Dependabot configuration with Rust support.
    Signed-off-by: pavelc4 <dimzzy23@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,13 +74,9 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
         lint.disable.add("NullSafeMutableLiveData")
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     buildFeatures {
@@ -102,6 +98,12 @@ android {
         jniLibs {
             useLegacyPackaging = false
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
 
