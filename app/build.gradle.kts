740bf62e11dbfb281c77d2ef2a30b2e2cb17b4f9
Gusti Aditya Muzaky <142430633+Gustyx-Power@users.noreply.github.com>
2025-12-27 14:45:35 +0700

    chore : Update versionName to '2.4-Debug-testdev'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 2
-        versionName = "2.3-Release"
+        versionName = "2.4-Debug-testdev"
 
         // Build date in format YYYY.MM.dd
         val buildDate = SimpleDateFormat("yyyy.MM.dd").format(Date())
