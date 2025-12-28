ee2569bc95e2d1dd8c22ce5b7b08d529156ecc00
Gustyx-Power <gustiadityamuzaky08@gmail.com>
2025-12-29 04:06:39 +0700

    chore : bump version to 2.4-Debug-Ci5-W1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 2
-        versionName = "2.4-Debug-Ci4-W1"
+        versionName = "2.4-Debug-Ci5-W1"
 
         // Build date in format YYYY.MM.dd
         val buildDate = SimpleDateFormat("yyyy.MM.dd").format(Date())
